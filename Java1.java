46aeaded6df8555671567a1e91f54165af3e3550
KM CHANCHAL <kmchanchal185@gmail.com>
2024-08-02 15:44:07 +0530

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,8 @@
+public class Java1 {
+    public static void main(String[] args) {
+        //output Hello, World.
+        //Hello, Java.
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
